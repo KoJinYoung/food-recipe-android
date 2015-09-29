c051ff59565466bd2b122502bfdf3c309ddc74c5
RyuHyeok <hotklh@hanmail.net>
2015-09-30 06:34:53 +0900

    thrift 적용 및 thrift 적용을 위한 gradle 설정 변경

--- app/src/main/java/com/MakSSe/foodRecipe/StartActivity.java
+++ app/src/main/java/com/MakSSe/foodRecipe/StartActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.foodRecipe.*;
 
 public class StartActivity extends ActionBarActivity {
 
